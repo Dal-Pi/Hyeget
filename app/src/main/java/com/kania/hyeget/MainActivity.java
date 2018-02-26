f54b202cabccb903565ffed2b96ee0ec7fc200a8
Dal-Pi <kimsh707@naver.com>
2018-02-26 23:28:23 +0900

    [180226][1.1] fix bug on high-resolution screen
    layout option changed
    get version info from package manager

--- app/src/main/java/com/kania/hyeget/MainActivity.java
+++ app/src/main/java/com/kania/hyeget/MainActivity.java
@@ -1,7 +1,10 @@
 package com.kania.hyeget;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textInfo = (TextView) findViewById(R.id.main_text_appinfo);
+        String infoPrefix = getString(R.string.app_info);
+        textInfo.setText(getInfoWithVersion(infoPrefix));
+    }
+
+    private String getInfoWithVersion(String prefix) {
+        String version;
+        try {
+            PackageInfo packageInfo =
+                    this.getPackageManager().getPackageInfo(this.getPackageName(), 0);
+            version = packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            version = "unknown";
+        }
+        return prefix + " " + version;
     }
 }
